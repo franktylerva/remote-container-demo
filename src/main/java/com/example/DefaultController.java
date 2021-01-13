bead1c7cd74933fa9a4edd068307d7603bfd9a95
ftyler <ftyler.ctr@kr.af.mil>
2021-01-13 19:34:52 +0000

    Use Java 11.

--- src/main/java/com/example/DefaultController.java
+++ src/main/java/com/example/DefaultController.java
@@ -1,12 +1,8 @@
 package com.example;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AllArgsConstructor;
 
 @RestController
 public class DefaultController {
