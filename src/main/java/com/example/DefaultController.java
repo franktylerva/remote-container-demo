7d6e8bff34e315d17a48b1a77588840e91553177
ftyler <ftyler.ctr@kr.af.mil>
2021-01-13 18:05:49 +0000

    Initial setup with Postgres.

--- src/main/java/com/example/DefaultController.java
+++ src/main/java/com/example/DefaultController.java
@@ -1,14 +1,22 @@
 package com.example;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 public class DefaultController {
     
+    @Autowired
+    private PersonRepository repository;
+
     @GetMapping("/")
-    public String index() {
-        return "Hello World from Remote Containers.";
+    public Iterable<Person> index() {
+        return repository.findAll();
     }
 
 }
--- src/main/java/com/example/RemoteContainerDemoApplication.java
+++ src/main/java/com/example/RemoteContainerDemoApplication.java
@@ -1,13 +1,26 @@
 package com.example;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class RemoteContainerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RemoteContainerDemoApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(PersonRepository repository) {
+		return args -> {
+			repository.save( new Person("Sally Ride") );
+			repository.save( new Person("Neil Armstrong") );
+			repository.save( new Person("Alan Shepard") );
+		};
 	}
 
 }
